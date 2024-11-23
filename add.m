e2edd7a0076816711333226d030a78732ff6df51
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-11-23 15:56:55 +0545

    Update add.m

--- add.m
+++ add.m
@@ -2,20 +2,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // Declare variables to store the numbers
         int number1, number2, sum;
         
-        // Input the two numbers
+        
         NSLog(@"Enter the first number: ");
         scanf("%d", &number1);
         
         NSLog(@"Enter the second number: ");
         scanf("%d", &number2);
         
-        // Calculate the sum
+    
         sum = number1 + number2;
         
-        // Output the result
+        
         NSLog(@"The sum of %d and %d is %d", number1, number2, sum);
     }
     return 0;
